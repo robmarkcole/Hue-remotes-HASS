489545fa32a8daad3d8b5d5d44d964b27bcb8cde
Eugenio Panadero <eugenio.panadero@gmail.com>
2020-03-14 15:57:28 +0100

    Add samples for all known remotes
    and also other examples like a geofence or a ZLLPresence used in tests

--- tests/api_samples.py
+++ tests/api_samples.py
@@ -14,6 +14,79 @@ MOCK_ZGP = {
     "uniqueid": "00:00:00:00:00:44:23:08-f2",
     "capabilities": {"certified": True, "primary": True, "inputs": []},
 }
+MOCK_FOH = {
+    "state": {"buttonevent": 21, "lastupdated": "2019-04-22T07:48:56"},
+    "swupdate": {"state": "notupdatable", "lastinstall": None},
+    "config": {"on": True},
+    "name": "Bed Switch",
+    "type": "ZGPSwitch",
+    "modelid": "FOHSWITCH",
+    "manufacturername": "PhilipsFoH",
+    "productname": "Friends of Hue Switch",
+    "diversityid": "ded6468f-6b26-4a75-9582-f2b52d36a5a3",
+    "uniqueid": "00:00:00:00:01:70:xx:xx-xx",
+    "capabilities": {
+        "certified": True,
+        "inputs": [
+            {
+                "repeatintervals": [],
+                "events": [
+                    {"buttonevent": 16, "eventtype": "initial_press"},
+                    {"buttonevent": 20, "eventtype": "short_release"},
+                ],
+            },
+            {
+                "repeatintervals": [],
+                "events": [
+                    {"buttonevent": 17, "eventtype": "initial_press"},
+                    {"buttonevent": 21, "eventtype": "short_release"},
+                ],
+            },
+            {
+                "repeatintervals": [],
+                "events": [
+                    {"buttonevent": 19, "eventtype": "initial_press"},
+                    {"buttonevent": 23, "eventtype": "short_release"},
+                ],
+            },
+            {
+                "repeatintervals": [],
+                "events": [
+                    {"buttonevent": 18, "eventtype": "initial_press"},
+                    {"buttonevent": 22, "eventtype": "short_release"},
+                ],
+            },
+        ],
+    },
+}
+MOCK_ROM = {
+    "state": {"buttonevent": 1002, "lastupdated": "2019-11-16T11:48:24"},
+    "swupdate": {"state": "noupdates", "lastinstall": "2019-11-16T10:54:58"},
+    "config": {"on": True, "battery": 100, "reachable": True, "pending": []},
+    "name": "Hue Smart button 2",
+    "type": "ZLLSwitch",
+    "modelid": "ROM001",
+    "manufacturername": "Philips",
+    "productname": "Hue Smart button",
+    "diversityid": "8b18a40c-eb6a-40d7-a0af-eb0906613d41",
+    "swversion": "2.21.0_r29784",
+    "uniqueid": "00:17:88:01:06:06:81:5c-01-fc00",
+    "capabilities": {
+        "certified": True,
+        "primary": True,
+        "inputs": [
+            {
+                "repeatintervals": [800],
+                "events": [
+                    {"buttonevent": 1000, "eventtype": "initial_press"},
+                    {"buttonevent": 1001, "eventtype": "repeat"},
+                    {"buttonevent": 1002, "eventtype": "short_release"},
+                    {"buttonevent": 1003, "eventtype": "long_release"},
+                ],
+            }
+        ],
+    },
+}
 MOCK_RWL = {
     "state": {"buttonevent": 4002, "lastupdated": "2019-12-28T21:58:02"},
     "swupdate": {"state": "noupdates", "lastinstall": "2019-10-13T13:16:15"},
@@ -59,6 +132,34 @@ MOCK_Z3_ROTARY = {
         ],
     },
 }
+MOCK_Z3_SWITCH = {
+    "state": {"buttonevent": 1002, "lastupdated": "2019-09-01T17:45:47"},
+    "swupdate": {"state": "noupdates", "lastinstall": "2019-09-01T15:26:15"},
+    "config": {"on": True, "battery": 100, "reachable": True, "pending": []},
+    "name": "Lutron Aurora 2",
+    "type": "ZLLSwitch",
+    "modelid": "Z3-1BRL",
+    "manufacturername": "Lutron",
+    "productname": "Lutron Aurora",
+    "diversityid": "2c3a75ff-55c4-4e4d-8c44-82d330b8eb9b",
+    "swversion": "3.1",
+    "uniqueid": "ff:ff:00:0f:e7:fe:95:cd-01-fc00",
+    "capabilities": {
+        "certified": True,
+        "primary": False,
+        "inputs": [
+            {
+                "repeatintervals": [800],
+                "events": [
+                    {"buttonevent": 1000, "eventtype": "initial_press"},
+                    {"buttonevent": 1001, "eventtype": "repeat"},
+                    {"buttonevent": 1002, "eventtype": "short_release"},
+                    {"buttonevent": 1003, "eventtype": "long_release"},
+                ],
+            }
+        ],
+    },
+}
 
 PARSED_ZGP = {
     "last_button_event": "3_click",
@@ -66,6 +167,23 @@ PARSED_ZGP = {
     "model": "ZGP",
     "name": "Hue Tap",
     "state": "3_click",
+}
+PARSED_FOH = {
+    "last_button_event": "left_lower_release",
+    "last_updated": ["2019-04-22", "07:48:56"],
+    "model": "FOH",
+    "name": "Bed Switch",
+    "state": "left_lower_release",
+}
+PARSED_ROM = {
+    "battery": 100,
+    "last_button_event": "1_click_up",
+    "last_updated": ["2019-11-16", "11:48:24"],
+    "model": "RWL",
+    "name": "Hue Smart button 2",
+    "on": True,
+    "reachable": True,
+    "state": "1_click_up",
 }
 PARSED_RWL = {
     "battery": 100,
@@ -87,4 +205,96 @@ PARSED_Z3_ROTARY = {
     "on": True,
     "reachable": True,
     "last_updated": ["2020-01-31", "15:56:19"],
+}
+PARSED_Z3_SWITCH = {
+    "last_button_event": "short_release",
+    "state": "short_release",
+    "last_updated": ["2019-09-01", "17:45:47"],
+}
+
+# Examples of other kind of sensors in Hue bridge (just for tests)
+MOCK_GEOFENCE = {
+    "state": {"presence": False, "lastupdated": "2019-04-09T06:05:00"},
+    "config": {"on": True, "reachable": True},
+    "name": "iPhone",
+    "type": "Geofence",
+    "modelid": "HA_GEOFENCE",
+    "manufacturername": "1ISn0hwg7oDVAmx4-gqDTN4eRR3ncfRl",
+    "swversion": "A_1",
+    "uniqueid": "L_02_iL4n7",
+    "recycle": False,
+}
+
+# Binary sensors
+MOCK_ZLLPresence = {
+    "state": {"presence": False, "lastupdated": "2020-02-06T07:28:08"},
+    "swupdate": {"state": "noupdates", "lastinstall": "2019-05-06T13:14:45"},
+    "config": {
+        "on": True,
+        "battery": 58,
+        "reachable": True,
+        "alert": "lselect",
+        "sensitivity": 2,
+        "sensitivitymax": 2,
+        "ledindication": False,
+        "usertest": False,
+        "pending": [],
+    },
+    "name": "Living room sensor",
+    "type": "ZLLPresence",
+    "modelid": "SML001",
+    "manufacturername": "Philips",
+    "productname": "Hue motion sensor",
+    "swversion": "6.1.1.27575",
+    "uniqueid": "00:17:88:01:02:00:af:28-02-0406",
+    "capabilities": {"certified": True, "primary": True},
+}
+MOCK_ZLLLightlevel = {
+    "state": {
+        "lightlevel": 0,
+        "dark": True,
+        "daylight": False,
+        "lastupdated": "2020-02-06T07:26:02",
+    },
+    "swupdate": {"state": "noupdates", "lastinstall": "2019-05-06T13:14:45"},
+    "config": {
+        "on": True,
+        "battery": 58,
+        "reachable": True,
+        "alert": "none",
+        "tholddark": 16000,
+        "tholdoffset": 7000,
+        "ledindication": False,
+        "usertest": False,
+        "pending": [],
+    },
+    "name": "Hue ambient light sensor 1",
+    "type": "ZLLLightLevel",
+    "modelid": "SML001",
+    "manufacturername": "Philips",
+    "productname": "Hue ambient light sensor",
+    "swversion": "6.1.1.27575",
+    "uniqueid": "00:17:88:01:02:00:af:28-02-0400",
+    "capabilities": {"certified": True, "primary": False},
+}
+MOCK_ZLLTemperature = {
+    "state": {"temperature": 1744, "lastupdated": "2020-02-06T07:26:26"},
+    "swupdate": {"state": "noupdates", "lastinstall": "2019-05-06T13:14:45"},
+    "config": {
+        "on": True,
+        "battery": 58,
+        "reachable": True,
+        "alert": "none",
+        "ledindication": False,
+        "usertest": False,
+        "pending": [],
+    },
+    "name": "Hue temperature sensor 1",
+    "type": "ZLLTemperature",
+    "modelid": "SML001",
+    "manufacturername": "Philips",
+    "productname": "Hue temperature sensor",
+    "swversion": "6.1.1.27575",
+    "uniqueid": "00:17:88:01:02:00:af:28-02-0402",
+    "capabilities": {"certified": True, "primary": False},
 }
