2d630cd8ed7e59581b41333d4ad39243e7a1046b
Eugenio Panadero <eugenio.panadero@gmail.com>
2020-03-18 14:49:48 +0100

    Limit minimum scan_interval to the default one
    In practice, the config parsing over `scan_interval: X` in the yaml
    is casting to int, so you cannot set 2.5 secs, or 0.25. BUT
    manually setting a 0.X value will be taken as scan_interval=0, which is bad.
    
    This avoids that scenario, by limiting to 0.5 secs (value to almost ensure 1Hz updates)

--- custom_components/hueremote/remote.py
+++ custom_components/hueremote/remote.py
@@ -5,7 +5,6 @@ import logging
 from aiohue.sensors import TYPE_ZGP_SWITCH, TYPE_ZLL_SWITCH
 from homeassistant.const import CONF_SCAN_INTERVAL
 from homeassistant.components.remote import PLATFORM_SCHEMA  # noqa: F401
-
 from homeassistant.helpers.event import async_track_time_interval
 
 from .implemented_remotes import (
@@ -33,10 +32,8 @@ REMOTE_NAME_FORMAT = "{}"  # just the given one in Hue app
 DEFAULT_SCAN_INTERVAL = timedelta(seconds=0.5)
 
 
-async def async_setup_platform(
-    hass, config, async_add_entities, discovery_info=None
-):
-    """Initialise Hue Bridge connection."""
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up remote platform by registering it on each Hue Bridge."""
     for bridge_entry_id, bridge in hass.data[HUE_DOMAIN].items():
 
         @callback
@@ -73,11 +70,13 @@ async def async_setup_platform(
 
         # Set up updates at scan_interval
         async def _update_remotes(now=None):
-            """Request a bridge data refresh to update states on remotes."""
+            """Request a bridge data refresh so remote states are updated."""
             await bridge.sensor_manager.coordinator.async_refresh()
             _LOGGER.debug("Update requested at %s", now)
 
-        remote_sc = config.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)
+        remote_sc = max(
+            DEFAULT_SCAN_INTERVAL, config.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)
+        )
         if remote_sc < bridge.sensor_manager.coordinator.update_interval:
             # Add listener to update remotes at high rate
             # TODO change sm.coordinator.update_interval instead.
